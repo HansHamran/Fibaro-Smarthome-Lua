7715539fc075ddbdbd2f305f8bc11e314080623d
Hans Olav Hamran <hans@hamran.com>
2017-05-25 21:12:15 +0200

    Create 219. Keypad press 1 lock door.lua

---
+++ 219. Keypad press 1 lock door.lua
@@ -0,0 +1,33 @@
+--[[
+%% properties
+%% weather
+%% events
+648 CentralSceneEvent 1
+%% globals
+--]]
+
+local startSource = fibaro:getSourceTrigger();
+if (
+ ( true )
+or
+startSource["type"] == "other"
+)
+then
+if fibaro:countScenes() > 1 then fibaro:abort() end
+ 
+  	-- Controlling is door is properly closed  (60 sec)
+    local teller = 1
+    while ( tonumber(fibaro:getValue(430, "value")) == 1 and teller < 60) do
+    fibaro:sleep(1000)
+    teller = teller + 1
+    end
+    -- Locking the door if it is closed but not locked
+  	if ( tonumber(fibaro:getValue(430, "value")) == 0 and tonumber(fibaro:getValue(441, "value")) == 1)
+    then 
+    fibaro:call(356, "secure");
+    end
+    
+end
+
+
+
