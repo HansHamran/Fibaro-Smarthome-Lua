efd432b0ae141dc856a2ae16411f39501430c145
Hans Olav Hamran <hans@hamran.com>
2018-05-20 18:59:39 +0200

    Update 029. Warning Heating is on and temp high.lua

--- 029. Warning Heating is on and temp high.lua
+++ 029. Warning Heating is on and temp high.lua
@@ -9,16 +9,17 @@
 
 local startSource = fibaro:getSourceTrigger();
 if (
- ( tonumber(fibaro:getValue(30, "value")) > 24 )
+ (( tonumber(fibaro:getValue(30, "value")) > 24 )
 and
  ( tonumber(fibaro:getValue(572, "value")) > 0 )
+and 	fibaro:getGlobal("Varmstue") == "false")
 or
 startSource["type"] == "other"
 )
 then
   	local melding = ("Stua er " .. fibaro:getValue(30, "value") .. " grader. Likevel står ovnene på.")
 	fibaro:call(448,'sendPush', melding);
-    fibaro:call(2, "sendEmail", "Stua er varm", melding); -- Mail Hans Olav
+    fibaro:call(2, "sendEmail", "Advarsel Stua er varm", melding); -- Mail Hans Olav
 end
 
 
