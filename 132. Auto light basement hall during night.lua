3f41b6cd66d68f2dc53c5f89fc42acc417f5205a
Hans Olav Hamran <hans@hamran.com>
2019-01-26 18:58:22 +0100

    Update 132. Auto light basement hall during night.lua

--- 132. Auto light basement hall during night.lua
+++ 132. Auto light basement hall during night.lua
@@ -9,10 +9,10 @@
 local startSource = fibaro:getSourceTrigger();
 if fibaro:countScenes() > 1 then fibaro:abort() end
 if (
- ( tonumber(fibaro:getValue(667, "value")) > 0  
+ ( tonumber(fibaro:getValue(745, "value")) > 0  
       and  fibaro:getGlobalValue("AutolysOFF") == "Ja"  
       and  fibaro:getGlobalValue("SunSet") == "Ja"  
-      and  tonumber(fibaro:getValue(669, "value")) < 50  
+      and  tonumber(fibaro:getValue(747, "value")) < 50  
       and  tonumber(fibaro:getValue(263, "value")) == 0  
       and  tonumber(fibaro:getValue(152, "value")) == 0  
       )
