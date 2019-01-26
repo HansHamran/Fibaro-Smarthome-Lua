96ad47914ccd95943024c0f614bc2dfdc176e039
Hans Olav Hamran <hans@hamran.com>
2019-01-26 19:25:18 +0100

    Update 220. Press keypad 2 Lock door and alarm.lua

--- 220. Press keypad 2 Lock door and alarm.lua
+++ 220. Press keypad 2 Lock door and alarm.lua
@@ -51,7 +51,7 @@ if fibaro:countScenes() > 1 then fibaro:abort() end
 			fibaro:call(444, "setArmed", "1");
     		fibaro:call(210, "setArmed", "1");
 			fibaro:call(343, "setArmed", "1");
-			fibaro:call(667, "setArmed", "1");
+			fibaro:call(745, "setArmed", "1");
 			fibaro:call(601, "setArmed", "1"); 
       
       		-- Turning off all lightning
